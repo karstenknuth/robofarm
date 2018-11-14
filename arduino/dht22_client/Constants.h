08124f128ac18bae10479c2378b41055b9c06504
Karsten Knuth <github@karstenknuth.de>
2018-11-14 12:22:22 +0100

    Restructure folders and add initial version of dht22 WIFI client.

---
+++ arduino/dht22_client/Constants.h
@@ -0,0 +1,5 @@
+static const char* ssid     = "PUT_WIFI_NAME_HERE";
+static const char* password = "PUT_WIFI_PWD_HERE";
+
+static const char* host = "PUT_IP_OF_RASPI_HERE";
+static const int httpPort = 5000;
